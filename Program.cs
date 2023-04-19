3a443455ae4f706b67574b15fb5de0157096a49f
LuizHen527 <125361452+LuizHen527@users.noreply.github.com>
2023-04-18 22:44:06 -0300

    terminei

--- Program.cs
+++ Program.cs
@@ -45,20 +45,20 @@
 // 	- case '2'
 // 	- Mostrar Method MostrarPassagens
 
-static char Menu()
-{
-    Console.WriteLine($"");
-    Console.WriteLine($@"Digite '1' para cadastrar passagens
-       '2' para listar passagens
-       '0' para Sair");
+//static char Menu()
+//{
+//    Console.WriteLine($"");
+//    Console.WriteLine($@"Digite '1' para cadastrar passagens
+//       '2' para listar passagens
+//       '0' para Sair");
 
-    return char.Parse(Console.ReadLine());
-}
+//    return char.Parse(Console.ReadLine());
+//}
 
 static bool login(int senha)
 {
     int senhaValida = 123456;
-    
+
     if (senha == senhaValida)
     {
         Console.WriteLine($"");
@@ -75,53 +75,75 @@ static bool login(int senha)
     }
 }
 
-string[] nome = new string[1];
-string[] origem = new string[1];
-string[] destino = new string[1];
-string[] dataVoo = new string[1];
+string[] nome = new string[5];
+string[] origem = new string[5];
+string[] destino = new string[5];
+string[] dataVoo = new string[5];
 
-static void CadastrarPassagens(string[] nome, string[] origem, string[] destino, string[] dataVoo)
+static int CadastrarPassagens(string[] nome, string[] origem, string[] destino, string[] dataVoo)
 {
-    for (int i = 0; i < 1; i++)
+    int numeroDePassagens = 0;
+
+    int i = 0;
+
+    bool PassagemOpcoes = false;
+
+        do
+        {
+            Console.WriteLine($"");
+            Console.WriteLine($"Insira seu nome:");
+            nome[i] = Console.ReadLine();
+
+            Console.WriteLine($"");
+            Console.WriteLine($"Insira sua origem:");
+            origem[i] = Console.ReadLine();
+
+            Console.WriteLine($"");
+            Console.WriteLine($"Insira um destino");
+            destino[i] = Console.ReadLine();
+
+            Console.WriteLine($"");
+            Console.WriteLine($"Insira a data que voce quer voar (dd/mm/aaaa):");
+            dataVoo[i] = Console.ReadLine();
+
+            Console.WriteLine($"");
+            Console.WriteLine($"Passagem Cadastrada");
+            Console.WriteLine($"");
+
+            i = i + 1;
+            numeroDePassagens = numeroDePassagens + 1;
+
+            if (5 > i)
+            {
+                Console.WriteLine($"Quer cadastrar mais uma passagem? ('S' para sim ou 'N' para não)");
+                char maisPassagem = char.Parse(Console.ReadLine().ToLower());
+                switch (maisPassagem)
+                {
+                    case 's':
+                        PassagemOpcoes = false; break;
+                    case 'n':
+                        PassagemOpcoes = true;
+                        i = 0;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+            Console.WriteLine("");
+
+           
+        } while ((PassagemOpcoes == false) && (5 > i));
+
+    if (i == 5)
     {
-        Console.WriteLine($"");
-        Console.WriteLine($"Insira seu nome:");
-        nome[i] = Console.ReadLine();
-
-        Console.WriteLine($"");
-        Console.WriteLine($"Insira sua origem:");
-        origem[i] = Console.ReadLine();
-
-        Console.WriteLine($"");
-        Console.WriteLine($"Insira um destino");
-        destino[i] = Console.ReadLine();
-        
-        Console.WriteLine($"");
-        Console.WriteLine($"Insira a data que voce quer voar (dd/mm/aaaa):");
-        dataVoo[i] = Console.ReadLine();
-
-        Console.WriteLine($"");
-        Console.WriteLine($"Passagem Cadastrada");
+        Console.WriteLine("Numero maximo de passagens cadastradas (5 passagens). Voce pode refaze-las, cadastrando novamente, se quiser.");
+        Console.WriteLine("");
     }
-}
 
-static void MostrarPassagens(string[] nome, string[] origem, string[] destino, string[] dataVoo)
-{
-    for (int i = 0; i < 1; i++)
-    {
-        Console.WriteLine($"Passagem numero {i+1}");
-        
-        Console.WriteLine($@"Nome: {nome[i]}");
+    return numeroDePassagens;
 
-        Console.WriteLine($@"Origem: {origem[i]}");
-
-        Console.WriteLine($@"Destino: {destino[i]}");
-
-        Console.WriteLine($@"Data do Voo: {dataVoo[i]}");
-        
-        Console.WriteLine($"");
-        
-    }
+    i = 0;
 }
 
 
@@ -136,18 +158,43 @@ do
 
 Console.WriteLine($"Bem vindo ao SENAI passagens aereas");
 
-switch (Menu())
+bool opcao = false;
+
+do
 {
-    case'1':
-    CadastrarPassagens(nome, origem, destino, dataVoo);
-    Menu();
-    break;
+    Console.WriteLine($"");
+    Console.WriteLine($@"Digite '1' para cadastrar passagens
+       '2' para listar passagens
+       '0' para Sair");
 
-    case '2':
-    MostrarPassagens(nome, origem, destino, dataVoo);
-    Menu();
-    break;
+    char menu = char.Parse(Console.ReadLine());
+    switch (menu)
+    {
+        case '1':
+            CadastrarPassagens(nome, origem, destino, dataVoo);
+            break;
 
-    default:
-    break;
-}
+        case '2':
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Passagem numero {i + 1}");
+
+                Console.WriteLine($@"Nome: {nome[i]}");
+
+                Console.WriteLine($@"Origem: {origem[i]}");
+
+                Console.WriteLine($@"Destino: {destino[i]}");
+
+                Console.WriteLine($@"Data do Voo: {dataVoo[i]}");
+
+                Console.WriteLine($"");
+            }
+            break;
+        case '0':
+            Console.WriteLine($"Fim");
+            opcao = true;
+            break;
+        default:
+            break;
+    }
+} while(opcao == false);
